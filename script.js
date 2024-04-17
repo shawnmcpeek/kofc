11535f0a2da1eb43103d9c03623ec3afd535cafb
Shawn McPeek <shawnmcpeek@gmail.com>
2024-04-16 20:11:42 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+// Function to generate FAQ cards
+function generateFaqCards() {
+  const faqContainer = document.getElementById("faqCards");
+
+  // Loop through faqData array and create cards
+  faqData.forEach((faq, index) => {
+    // Create the main card container
+    const faqCard = document.createElement("div");
+    faqCard.classList.add("faq-card");
+    faqCard.onclick = function () {
+      toggleAnswer(index + 1);
+    };
+
+    // Create question paragraph
+    const question = document.createElement("p");
+    question.innerHTML = `<strong>${faq.question}</strong>`;
+    faqCard.appendChild(question);
+
+    // Create answer paragraph
+    const answer = document.createElement("div");
+    answer.classList.add("answer");
+    answer.id = `answer${index + 1}`;
+    answer.innerHTML = `<p>${faq.answer}</p>`;
+    faqCard.appendChild(answer);
+
+    // Append the card to the faqContainer
+    faqContainer.appendChild(faqCard);
+  });
+}
+
+// Toggle answer visibility
+function toggleAnswer(faqId) {
+  var answer = document.getElementById("answer" + faqId);
+  answer.classList.toggle("show");
+}
+
+// Generate FAQ cards when the page loads
+window.onload = generateFaqCards;
---
+++ textdata.js
@@ -0,0 +1,60 @@
+const faqData = [
+  {
+    question: "What is the Knights of Columbus?",
+    answer: `
+      <p>The Knights of Columbus is a Catholic Fraternal Organization created to serve families, parishes, community, and the Catholic Church. The first Knights of Columbus were started in 1882 in New Haven, CT by Fr. Michael McGivney.</p>
+      <p>At the time, Catholics could not get life insurance, and as most of the men/breadwinners worked in dangerous hard labor jobs, many families would be left without money to support them if the husband died. After the father's death, the families would be torn apart and the children would be fostered by other families who most likely weren't Catholic and wouldn't raise the children in their family's faith. McGivney and his first knights banded together and started a small fund to give to those who need support after the loss of the husband. They also started providing community and parish support in many different ways.</p>
+      <p>Since that time, the Order has grown from nine men in one local council to over two million men in over 16,000 councils and over 10 countries, including the United States, Canada, France, The Philippines, Mexico, Poland, Ukraine, and South Korea.</p>
+    `,
+  },
+  {
+    question: "Who are Knights of Columbus?",
+    answer: `
+      <p>The Knights of Columbus are practicing Catholic men who are 18 years and older. In your local council, they are men you see every day at mass and events. They are fathers, sons, and men who love their families and parish. The knights and their families volunteer their time, talent, and 'treasure' to raise funds to serve their parish and community. Beside our Catholic faith, the knights follow four tenets: Charity, Unity, Fraternity, and Patriotism.</p>
+    `,
+  },
+  {
+    question: "What does Council #15857 do?",
+    answer: `
+      <p>The Knights of Council #15857 are active in many ways. Some are more visible, while others may not be. Some of the most visible ways that the council serves are in the monthly breakfast burrito sales, canned food drives, lenten fish fry, annual Red Hidden Peach sales and sweet corn sales, and hosting the monthly parish Game Night and Movie Night. You'll almost always find knights helping set up and put away tables and chairs for parish events, as well as the canopy tent on the McGivney Terrace, on the south side of the parish. They will also cook food for parish picnics and other events.</p>
+      <p>There are many ways the council serves that may not be seen. The council uses funds raised through events like peaches and sweet corn sales to support:</p>
+      <ul>
+        <li>Coats for Kids (winter coats for local kids that need one)</li>
+        <li>Unbound (support for a young child, currently located in Madagascar)</li>
+        <li>Seminarians and Diaconate candidates</li>
+        <li>Canned Food Drives for local food banks</li>
+        <li>Rosaries for funerals</li>
+        <li>Pregnancy Support Centers</li>
+        <li>March for Life and more!</li>
+      </ul>
+    `,
+  },
+  {
+    question: "How do I support the Knights of Columbus?",
+    answer: `
+      <p>You are probably already supporting the Knights. If you buy breakfast burritos, peaches, or sweet corn, you are helping support our causes! Join us for one of our events. We would love to see many more people playing games at Game Night or enjoying popcorn and a movie at Movie Night. All of these are ways to support the Knights.</p>
+    `,
+  },
+  {
+    question: "How do I join the Knights of Columbus?",
+    answer: `
+      <p>We are so happy to hear you want to join the Knights! There are two ways to join the Knights:</p>
+      <ol>
+        <li>Go to <button class="popup-button" onclick="window.open('https://www.kofc.org')">KofC</button>
+        and click on 'Join'. This will take a few minutes but is the fastest and easiest method. Make sure you put our council number, 15857 in. There is a code on the page to even get your first year membership free! Once you complete the registration, you are already a knight! If we haven't already been talking with you, we will get in contact with you to help you join the council more.</li>
+        <li>Fill out a Form 100 that you can get from many of the knights or download <button class="popup-button" onclick="window.open('link_to_form')">here</button>, and give to a knight or even turn in to the parish office. Someone will contact you to help you finish becoming a knight. Whether you join the knights online or with a Form 100, you will go through a small ceremony to explain our tenets of Charity, Unity, and Fraternity. Your family is invited and welcome to watch the ceremony with you. This can also be watched/attended online if scheduling is an issue.</li>
+      </ol>
+    `,
+  },
+  {
+    question: "What do I have to do as a knight? What am I required to do?",
+    answer: `
+      <p>Once you are a knight, there is no requirement of how much or how little you need to participate in council events. The only real 'requirement' is paying the annual council dues, and if you have financial issues that make that hard, talk to the officers; we can help!</p>
+      <p>We recommend to all our knights the same thing if they aren't sure of what to do as a knight. Help with one thing, once a year. There are many programs that can use knights doing small things. When we hold a canned food drive, help hand out the 'shopping list' and bags after the mass you already attend. Help sell burritos or sweet corn or peaches, again after the mass you are already at! These services take fifteen minutes or less of your day and don't require you to make an extra trip anywhere.</p>
+      <p>Want a real stress-free way to participate? Join at a game night or movie night! What do you have to do? Play games or watch a movie! Buy a FundCrazr ticket (or more than one).</p>
+      <p>As a council member, you'll receive emails about upcoming and ongoing events. If you see one that interests you, sign up and participate. Do you think you have a skill that could help (are you good with numbers and financials? Think you can help get more men interested in the Knights? Are you passionate about a program or area we already help in?) and want to serve the council in that way? Talk to an officer at the meeting.</p>
+      <p>Is there something you would like to see the Knights participate in or help with? Talk to the council at a meeting. We are always interested in new ways we can help.</p>
+      <p>Any man that joins is a candidate to be an officer in the council. Men like you are qualified to be an officer and help the council. No man is 'not good enough' to be an officer. No matter how you find a way to serve, we are happy to see you.</p>
+    `,
+  },
+];
